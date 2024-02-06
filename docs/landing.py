70e5f07037d69798336ea4e1613e526591d29d70
renardeinside <polarpersonal@gmail.com>
2024-02-06 02:11:58 +0100

    simplify landing

--- docs/landing.py
+++ docs/landing.py
@@ -27,41 +27,33 @@ LINKS = [
         "Concepts",
     ),
 ]
-
-ImageContainer = Div.derive(
-    classes=Classes("max-w-md m-4"),
-    image=Img.factory(
-        file_path=Path(__file__).parent.parent / Path("raw/with_text.svg"),
-        alt="Schorle logo",
-        mime_type="image/svg+xml",
-    ),
-)
-
-Headline = Div.derive(
-    classes=Classes("text-2xl font-bold p-4 w-5/6 text-center"),
-    text="""Pythonic Server-Driven UI Kit for building modern apps.""",
-)
-
 LinkWithIcon = Link.derive(icon=Icon.factory())
 
 wrapped_links = [
     LinkWithIcon(href=link, icon=icon, text=text, classes=Classes("btn btn-primary")) for link, icon, text in LINKS
 ]
 
-Buttons = Div.derive(
-    classes=Classes("flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-4"),
-    buttons=(list[LinkWithIcon], Field(default_factory=lambda: wrapped_links)),
-)
-
-Content = Div.derive(
-    classes=Classes("max-w-screen-md flex flex-col justify-center items-center h-5/6 space-y-4"),
-    image_container=ImageContainer.factory(),
-    headline=Headline.factory(),
-    buttons=Buttons.factory(),
-)
-
 ContentWrapper = Div.derive(
-    classes=Classes("flex flex-col justify-center items-center h-screen"), content=Content.factory()
+    classes=Classes("flex flex-col justify-center items-center h-screen"),
+    content=Div.derive(
+        classes=Classes("max-w-screen-md flex flex-col justify-center items-center h-5/6 space-y-4"),
+        image_container=Div.derive(
+            classes=Classes("max-w-md m-4"),
+            image=Img.factory(
+                file_path=Path(__file__).parent.parent / Path("raw/with_text.svg"),
+                alt="Schorle logo",
+                mime_type="image/svg+xml",
+            ),
+        ).factory(),
+        headline=Div.derive(
+            classes=Classes("text-2xl font-bold p-4 w-5/6 text-center"),
+            text="""Pythonic Server-Driven UI Kit for building modern apps.""",
+        ).factory(),
+        buttons=Div.derive(
+            classes=Classes("flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-4"),
+            buttons=(list[LinkWithIcon], Field(default_factory=lambda: wrapped_links)),
+        ).factory(),
+    ).factory(),
 )
 
 
