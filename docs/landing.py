b2ed8008bb9c2f5bbabc060e3d94701f94c2e71c
renardeinside <polarpersonal@gmail.com>
2024-02-13 19:51:26 +0100

    fix styles

--- docs/landing.py
+++ docs/landing.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from functools import partial
 from pathlib import Path
 
@@ -7,6 +5,7 @@ from pydantic import Field
 
 from schorle.app import Schorle
 from schorle.classes import Classes
+from schorle.component import Component
 from schorle.element import a, div, img, link, p
 from schorle.icon import Icon
 from schorle.page import Page
@@ -18,13 +17,25 @@ app = Schorle(
     ]
 )
 
+
+class LinkWithIcon(Component):
+    href: str
+    icon_name: str
+    text: str
+
+    def render(self):
+        with a(href=self.href, classes=Classes("btn btn-primary font-normal")):
+            text(self.text)
+            Icon(name=self.icon_name).add()
+
+
 LINKS = [
-    ("https://github.com/renardeinside/schorle", Icon(name="github"), "GitHub"),
-    ("https://github.com/renardeinside/schorle/tree/main/examples", Icon(name="code"), "Examples"),
-    (
-        "https://medium.com/@polarpersonal/schorle-testing-the-waters-with-a-python-server-driven-ui-kit-053f85ee6574",
-        Icon(name="book"),
-        "Concepts",
+    LinkWithIcon(href="https://github.com/renardeinside/schorle", icon_name="github", text="GitHub"),
+    LinkWithIcon(href="https://github.com/renardeinside/schorle/tree/main/examples", icon_name="code", text="Examples"),
+    LinkWithIcon(
+        href="https://medium.com/@polarpersonal/schorle-testing-the-waters-with-a-python-server-driven-ui-kit-053f85ee6574",
+        icon_name="book-marked",
+        text="Concepts",
     ),
 ]
 
@@ -39,16 +50,14 @@ class LandingPage(Page):
                     src=Path(__file__).parent.parent / Path("raw/with_text.svg"),
                     alt="Schorle logo",
                     mime_type="image/svg+xml",
-                    classes=Classes("max-w-md m-4"),
+                    classes=Classes("w-3/4"),
                 )
-            with p(classes=Classes("text-2xl font-bold p-4 w-5/6 text-center")):
+            with p(classes=Classes("text-2xl m-4 p-4 w-5/6 text-center")):
                 text("Pythonic Server-Driven UI Kit for building modern apps.")
 
             with div(classes=Classes("flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-4")):
-                for href, icon, _text in LINKS:
-                    with a(href=href, classes=Classes("btn btn-primary font-normal")):
-                        text(_text)
-                        icon.add()
+                for _link in LINKS:
+                    _link()
 
 
 @app.get("/")
