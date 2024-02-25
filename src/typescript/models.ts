75e1cf79fa3075d41d76863d74a9afc5df2d0583
Ivan Trusov <polarpersonal@gmail.com>
2024-02-25 13:18:11 +0100

    ✨ add prettier && add retry logic for dev mode (#16)
    ✨ add prettier && add retry logic for dev mode

--- src/typescript/models.ts
+++ src/typescript/models.ts
@@ -1,19 +1,18 @@
-
 interface ClientMessage {
-    trigger: string;
-    target: string;
-    value: string | null;
+  trigger: string;
+  target: string;
+  value: string | null;
 }
 
 enum Action {
-    morph = 'morph',
-    clear = 'clear',
+  morph = 'morph',
+  clear = 'clear'
 }
 
 interface ServerMessage {
-    target: string;
-    payload: string;
-    action: Action;
+  target: string;
+  payload: string;
+  action: Action;
 }
 
 export { ClientMessage, ServerMessage, Action };
