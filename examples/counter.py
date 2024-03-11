36a1eb7da73355a7d23af848f05369a4a9128d91
renardeinside <polarpersonal@gmail.com>
2024-03-11 23:16:43 +0100

    finish removing dynamicity

--- examples/counter.py
+++ examples/counter.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from schorle.app import Schorle
 from schorle.attrs import Classes
 from schorle.button import Button
-from schorle.component import Component
 from schorle.element import div
 from schorle.page import Page
 from schorle.text import text
@@ -12,18 +11,16 @@ from schorle.theme import Theme
 app = Schorle(theme=Theme.AUTUMN)
 
 
-class StatefulButton(Component):
-
-    def render(self):
-        with Button(modifier="primary"):
-            text("Click me")
+def SampleButton():
+    with Button(modifier="primary"):
+        text("Click me")
 
 
 class PageWithButton(Page):
 
     def render(self):
         with div(classes=Classes("flex flex-col justify-center items-center h-screen")):
-            StatefulButton()
+            SampleButton()
 
 
 @app.get("/")
