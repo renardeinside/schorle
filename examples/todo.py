da1439818b1ba87f66bccdae28016bd64c221222
renardeinside <polarpersonal@gmail.com>
2024-04-24 00:26:01 +0200

    add loading logic

--- examples/todo.py
+++ examples/todo.py
@@ -1,3 +1,4 @@
+import asyncio
 from functools import partial
 
 from pydantic import BaseModel, Field
@@ -6,6 +7,7 @@ from schorle.app import Schorle
 from schorle.attrs import Bind, On
 from schorle.component import Component
 from schorle.element import button, div, input_
+from schorle.icon import icon
 from schorle.reactive import Reactive
 from schorle.text import text
 
@@ -14,6 +16,7 @@ app = Schorle(title="Schorle | Todo App")
 
 class TodoState(BaseModel):
     current: Reactive[str] = Field(default_factory=Reactive.factory(""))
+    loading: Reactive[bool] = Field(default_factory=Reactive.factory(False))
     todos: Reactive[list[str]] = Field(
         default_factory=Reactive.factory(
             [
@@ -29,7 +32,9 @@ class TodoState(BaseModel):
             await self.current.set("")  # Clear input field
 
     async def remove(self, index: int):
-        await self.todos.set([*self.todos.rx[:index], *self.todos.rx[index + 1 :]])
+        async with self.loading.ctx(True):
+            await asyncio.sleep(1)  # Simulate network request
+            await self.todos.set([*self.todos.rx[:index], *self.todos.rx[index + 1 :]])
 
 
 class Todos(Component):
@@ -37,6 +42,7 @@ class Todos(Component):
 
     def initialize(self):
         self.state.todos.subscribe(self.rerender)
+        self.state.loading.subscribe(self.rerender)
 
     def render(self):
         with div(classes="flex flex-col space-y-2"):
@@ -47,18 +53,22 @@ class Todos(Component):
                     bind=Bind("value", self.state.current),
                 )
                 with button(on=On("click", self.state.add), classes="btn btn-primary"):
-                    text("Add")
+                    icon(name="list-plus")
+
         with div(classes="flex flex-col space-y-2"):
-            for index, todo in enumerate(self.state.todos.rx):
-                with div(classes="flex space-x-4"):
-                    with div(classes="text-lg align-middle font-semibold w-48"):
-                        text(todo)
-                    with button(on=On("click", partial(self.state.remove, index)), classes="btn btn-secondary"):
-                        text("Remove")
+            if self.state.loading.rx:
+                div(classes="w-full min-h-16 flex skeleton bg-base-300")
+            else:
+                for index, todo in enumerate(self.state.todos.rx):
+                    with div(classes="flex space-x-4"):
+                        with div(classes="text-lg align-middle font-semibold w-48"):
+                            text(todo)
+                        with button(on=On("click", partial(self.state.remove, index)), classes="btn btn-secondary"):
+                            icon(name="square-check")
 
 
 class HomePage(Component):
-    classes: str = "flex flex-col items-center justify-center h-screen"
+    classes: str = "flex flex-col items-center justify-center h-screen space-y-4"
 
     def render(self):
         Todos()
