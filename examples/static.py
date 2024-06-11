d322cce5f905e57310b218a24616b6149549edbf
renardeinside <polarpersonal@gmail.com>
2024-06-11 21:44:12 +0200

    fix static example

--- examples/static.py
+++ examples/static.py
@@ -1,20 +1,19 @@
 from schorle.app import Schorle
-from schorle.component import Component
-from schorle.element import h1, span
+from schorle.component import component
+from schorle.element import span
 from schorle.icon import icon
 from schorle.text import text
 
 app = Schorle()
 
 
-class ExamplePage(Component):
-    def render(self):
-        with h1(classes="text-2xl w-96 flex justify-center items-center p-4"):
-            icon(name="party-popper")
-            with span():
-                text("Welcome to Schorle!")
+@component(classes="text-2xl w-96 flex justify-center items-center p-4", tag="h1")
+def example():
+    icon(name="party-popper")
+    with span():
+        text("Welcome to Schorle!")
 
 
 @app.get("/")
 def home():
-    return ExamplePage()
+    return example()
